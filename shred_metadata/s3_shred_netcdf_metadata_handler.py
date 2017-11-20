616711bbf257da81ab57dd13650ebf22e98a9129
ketaki.sharma <ketaki.sharma@metoffice.gov.uk>
2017-11-20 16:32:47 +0000

    no message

--- shred_metadata/s3_shred_netcdf_metadata_handler.py
+++ shred_metadata/s3_shred_netcdf_metadata_handler.py
@@ -22,6 +22,15 @@ def _get_geo_area_from_dataset(ds):
 def _get_forecast_date_from_dataset(ds):
     return ds.data_vars['issue_time'].values
 
+def _get_ddb_geojson_from_coordinates(self,coordinate_list):
+    coords = []
+    for i in coordinate_list:
+        lon = {"N": str(i[0])}
+        lat = {"N": str(i[1])}
+        coords.append ({"L": [lon, lat]})
+    print(coords)
+    return coords
+
 
 class S3ShredNetcdfMetadataHandler(AWSLambdaBase):
 
@@ -40,18 +49,39 @@ class S3ShredNetcdfMetadataHandler(AWSLambdaBase):
             bucket_name,
             object_key,
             file_path)
+        print(xarray.open_dataset(file_path))
         return xarray.open_dataset(file_path)
-
 
 
     def _handle(self, event, context):
         records = event['Records']
         for record in records:
             bucket_name = _get_s3_bucket_name_for_record(record)
+            print("bucket name is: " + bucket_name)
             file_name = _get_s3_key_name_from_record(record)
+            print("file name is: " + file_name)
+            data_set  = self._get_netcdf_data_for_file(bucket_name, file_name)
+            print(data_set )
+            self._send_metadata_to_dynamo_db(data_set,bucket_name,file_name)
+            data_set.close()
 
-        ds = self._get_netcdf_data_for_file(bucket_name, file_name)
-        print(ds)
+    def _send_metadata_to_dynamo_db(self,ds,bucket_name,file_name):
+        geojson_object = _get_geo_area_from_dataset(ds)
+
+        dynamodb = boto3.resource('dynamodb' , region_name='eu-west-1')
+        file_id = uuid.uuid4().get_hex()
+        print(file_id)
+
+        table = dynamodb.Table('netcdf-metadata-table')
+        table.put_item(
+            Item={
+                'bulk_forecast_file_id': file_id,
+                'forecast_date': _get_forecast_date_from_dataset(ds),
+                'bucket_name' : bucket_name,
+                'key': file_name,
+                'geo_json':_get_ddb_geojson_from_coordinates(geojson_object['coordinates'][0])
+            }
+        )
 
 
 handler = S3ShredNetcdfMetadataHandler.get_handler(boto3.client('s3'))
