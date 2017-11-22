2f6647975314c3453146953c64adf95170a49e4d
ketaki.sharma <ketaki.sharma@metoffice.gov.uk>
2017-11-22 17:40:50 +0000

    .

--- shred_metadata/s3_shred_netcdf_metadata_handler.py
+++ shred_metadata/s3_shred_netcdf_metadata_handler.py
@@ -64,17 +64,18 @@ class S3ShredNetcdfMetadataHandler(AWSLambdaBase):
         return xarray.open_dataset(file_path)
 
     def _shred_metadata(self, event):
-        records = event['Records']
-        for record in records:
+
+        for record in event['Records']:
             bucket_name = _get_s3_bucket_name_for_record(record)
             print("bucket name is: " + bucket_name)
             file_name = _get_s3_key_name_from_record(record)
             print("file name is: " + file_name)
-            data_set  = self._get_netcdf_data_for_file(bucket_name, file_name)
+            data_set = self._get_netcdf_data_for_file(bucket_name, file_name)
             print('The data_set is*****************' + data_set)
-            self._send_metadata_to_dynamo_db(data_set,bucket_name,file_name)
+            self._send_metadata_to_dynamo_db(data_set, bucket_name, file_name)
 
-    def _send_metadata_to_dynamo_db(data_set,bucket_name,file_name):
+    @staticmethod
+    def _send_metadata_to_dynamo_db(data_set, bucket_name,file_name):
 
         dynamodb = boto3.resource('dynamodb' , region_name='eu-west-1')
         file_id = uuid.uuid4().get_hex()
@@ -84,21 +85,20 @@ class S3ShredNetcdfMetadataHandler(AWSLambdaBase):
         table.put_item(
             Item={
                 'uuid': str(uuid.uuid4()),
-                'bucket_name' : bucket_name,
+                'bucket_name': bucket_name,
                 'key_name': file_name,
                 'forecast_date': _get_forecast_date_from_dataset(data_set),
                 'geo_json': {
-                    'type' : _get_type(data_set),
+                    'type': _get_type(data_set),
                     'crs': {
                         'properties': {
                              'name': _get_properties(data_set)
                          },
-                        'coordinates': [_get_coordinates(data_set)],
+                        'coordinates': [_get_coordinates(data_set)]
                     }
                 }
             }
         )
-
 
     def _handle(self, event, context):
         self._shred_metadata(event)
@@ -106,7 +106,5 @@ class S3ShredNetcdfMetadataHandler(AWSLambdaBase):
             'statusCode': 200,
             'body': 'metadata added to database'
         })
-
-
 
 handler = S3ShredNetcdfMetadataHandler.get_handler(boto3.client('s3'))
