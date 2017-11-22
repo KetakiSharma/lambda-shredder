d2adb4d4f21339031ef670821ebe7516ea13c484
ketaki.sharma <ketaki.sharma@metoffice.gov.uk>
2017-11-22 16:49:04 +0000

    .

--- shred_metadata/s3_shred_netcdf_metadata_handler.py
+++ shred_metadata/s3_shred_netcdf_metadata_handler.py
@@ -1,12 +1,11 @@
 import uuid
 import boto3
-import logging
+from decimal import Decimal
 import os
 import json
 import xarray
 
 from lambda_base.aws_lambda_base import AWSLambdaBase
-
 
 def _get_s3_bucket_name_for_record(record):
     return record['s3']['bucket']['name']
@@ -16,30 +15,31 @@ def _get_s3_key_name_from_record(record):
     return record['s3']['object']['key']
 
 
-def _get_geo_area_from_dataset(data_set):
-    return json.loads(data_set.attrs['core_geographic_area_shape'])
-
-
 def _get_forecast_date_from_dataset(data_set):
     return str(data_set.data_vars['issue_time'].values)
 
 
-def _get_type(data_set):
-    print(json.loads(data_set.attrs['core_geographic_area_shape']))
-    return json.loads(data_set.attrs['core_geographic_area_shape'])['type']
+def _get_properties_geojson(data_set):
+    return json.loads(data_set.attrs['core_geographic_area_shape'])
+
 
 def _get_properties(data_set):
-    return json.loads(data_set.attrs['core_geographic_area_shape'])['properties']
+    return _get_properties_geojson(data_set)['crs']['properties']['name']
+
+def _get_type(data_set):
+    return _get_properties_geojson(data_set)['type']
 
 
 def _get_coordinates(data_set):
-    properties_geojson = json.loads(data_set.attrs['core_geographic_area_shape'])
+    properties_geojson = _get_properties_geojson(data_set)
+
     coordinate_list = properties_geojson['coordinates'][0]
+
     coordinates = []
-    for i in coordinate_list:
-        lon = {"N": str(i[0])}
-        lat = {"N": str(i[1])}
-        coordinates.append ({"L": [lon, lat]})
+    for coord in coordinate_list:
+        lon = Decimal(str(coord[1]))
+        lat = Decimal(str(coord[0]))
+        coordinates.append ([lon, lat])
     print(coordinates)
     return coordinates
 
@@ -63,9 +63,19 @@ class S3ShredNetcdfMetadataHandler(AWSLambdaBase):
         print(xarray.open_dataset(file_path))
         return xarray.open_dataset(file_path)
 
+    def _shred_metadata(self, event):
+        records = event['Records']
+        for record in records:
+            bucket_name = _get_s3_bucket_name_for_record(record)
+            print("bucket name is: " + bucket_name)
+            file_name = _get_s3_key_name_from_record(record)
+            print("file name is: " + file_name)
+            data_set  = self._get_netcdf_data_for_file(bucket_name, file_name)
+            print('The data_set is*****************' + data_set)
+            self._send_metadata_to_dynamo_db(data_set,bucket_name,file_name)
+
     def _send_metadata_to_dynamo_db(self,data_set,bucket_name,file_name):
-        geojson_object = _get_geo_area_from_dataset(data_set)
-        print('the geojson object looks like this **********' + geojson_object)
+
         dynamodb = boto3.resource('dynamodb' , region_name='eu-west-1')
         file_id = uuid.uuid4().get_hex()
         print(file_id)
@@ -76,12 +86,12 @@ class S3ShredNetcdfMetadataHandler(AWSLambdaBase):
                 'uuid': str(uuid.uuid4()),
                 'bucket_name' : bucket_name,
                 'key_name': file_name,
-                'forecast_date': str(_get_forecast_date_from_dataset(data_set)),
+                'forecast_date': _get_forecast_date_from_dataset(data_set),
                 'geo_json': {
-                    'type' : str(_get_type(data_set)),
+                    'type' : _get_type(data_set),
                     'crs': {
                         'properties': {
-                             'name': str(_get_properties(data_set))
+                             'name': _get_properties(data_set)
                          },
                         'coordinates': [_get_coordinates(data_set)],
                     }
@@ -91,16 +101,12 @@ class S3ShredNetcdfMetadataHandler(AWSLambdaBase):
 
 
     def _handle(self, event, context):
-        records = event['Records']
-        for record in records:
-            bucket_name = _get_s3_bucket_name_for_record(record)
-            print("bucket name is: " + bucket_name)
-            file_name = _get_s3_key_name_from_record(record)
-            print("file name is: " + file_name)
-            data_set  = self._get_netcdf_data_for_file(bucket_name, file_name)
-            print('The data_set is*****************' + data_set)
-            self._send_metadata_to_dynamo_db(data_set,bucket_name,file_name)
-            data_set.close()
+        self._shred_metadata(event)
+        return ({
+            'statusCode': 200,
+            'body': 'metadata added to database'
+        })
+
 
 
 handler = S3ShredNetcdfMetadataHandler.get_handler(boto3.client('s3'))
